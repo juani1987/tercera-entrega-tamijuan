24ba9ca67bae3610ba4b0dacbba9145b0e05cfdd
Juan <tamijuan94@gmil.com>
2024-08-06 14:22:47 -0300

    update

--- misdatos/views.py
+++ misdatos/views.py
@@ -6,6 +6,12 @@ from django.urls import reverse_lazy
 from.moduls import*
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+
 
 
 from.forms import *
